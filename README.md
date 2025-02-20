df43d7a03bdeb1119ca54145e9f57b234aea4199
锤子啊 <xinlei_coder@aliyun.com>
2025-02-20 11:59:16 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# move_sync
+
+运动社交App (慕动)
+
+
+# 开发环境概述
+
+- 语言：Dart
+- 框架：Flutter 3.29.0
+- IDE：Android Studio
---
+++ android/app/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("xyz.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "xyz.icxl.ms"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+       applicationId = "xyz.icxl.ms"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
